fe8451ac97b799d1b04e150a652137cf5cd63d3e
Ismael Rodrigues <irx800@gmail.com>
2025-09-01 16:55:25 +0000

    Adiciona scripts 'lint:check' e 'lint:fix'

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnewsV2
+
 Implementação do https://www.tabnews.com.br para o https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 function Home() {
   return <h1>Olá! Eu sou o TabNewsV2.</h1>;
 }
+
 export default Home;
